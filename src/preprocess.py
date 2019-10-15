698f8d001675c448fa50f75a05cf403492ab2bb7
Zhiming <stevenbirdbrown1998@gmail.com>
2019-10-16 00:40:58 +0800

    finish load data to dataset, dataloader to go

--- src/preprocess.py
+++ src/preprocess.py
@@ -5,6 +5,7 @@ from mxnet import nd
 import multiprocessing as mp
 from collections import defaultdict
 from bert.embedding import BertEmbedding
+from ast import literal_eval
 
 logger = logging.Logger(__name__)
 logger.setLevel(logging.WARNING)
@@ -28,19 +29,33 @@ def load_sentences(filepath, keys=['obs1', 'obs2', 'hyp1', 'hyp2']):
     raw_data = load_jsonl(filepath)
     ret = defaultdict(list)
     logger.info('Build the dictionary for each kind of sentences')
-    [[ret[key].append(entry[key]) for key in keys] for entry in raw_data]
+    for entry in raw_data:
+        for key in keys:
+            ret[key].append(entry[key])
+    return list(ret.values())
+
+def load_labels(filepath):
+    '''
+    this function will load the given label from `filepath`
+    '''
+    with open(filepath, 'r') as f:
+        ret = [0 if literal_eval(line)==1 else 1 for line in f]
     return ret
 
-def to_dataloader(sentences, ctx=mx.gpu(), batch_size=32, max_seq_length=20):
+def to_dataset(sentences, labels, ctx=mx.gpu(), batch_size=32, max_seq_length=20):
     '''
-    this function will use BertEmbedding to get dataloader for each
-    kind of sentences
+    this function will use BertEmbedding to get each fields' embeddings
+    and load the given labels, put them together into a dataset
     '''
     bertembedding = BertEmbedding(ctx=ctx, batch_size=batch_size, max_seq_length=max_seq_length)
     logger.info('Construct bert embedding for these fields')
+    
+    embs = []
     for sts in sentences:
         tokens_embs = bertembedding.embedding(sts)
-        embs = [nd.array(token_emb[1]) for token_emb in tokens_embs]
-
-
+        embs.append([nd.array(token_emb[1]) for token_emb in tokens_embs])
+    
+    dataset = [[obs1, obs2, hyp1, hyp2, label] for obs1, obs2, hyp1, hyp2, label in \
+               zip(*embs, labels)]
+    return dataset
 
